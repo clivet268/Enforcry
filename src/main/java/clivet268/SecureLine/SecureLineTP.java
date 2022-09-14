e59e4936e110dd1f1c1fc1863a14718fa3b2ef5a
clivet268 <clivet268@gmail.com>
2022-09-14 12:16:57 -0400

    Getting of the PIK information securely and without repeating ideally

--- src/main/java/clivet268/SecureLine/SecureLineTP.java
+++ src/main/java/clivet268/SecureLine/SecureLineTP.java
@@ -1,8 +1,11 @@
 package clivet268.SecureLine;
 
 import clivet268.Enforcry;
+import clivet268.FileEncryption.EncrypterDecrypter;
 import clivet268.SecureLine.Commands.ExacutableCommand;
 import org.apache.commons.lang3.ArrayUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -11,10 +14,21 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static clivet268.Util.Univ.*;
 
 public class SecureLineTP {
+
+    public static String[] o3ut = new String[0];
+
+    public static String keylayeri(String in, String k){
+        return EncrypterDecrypter.encrypt(in, k);
+    }
+
+    public static String keylayero(String in, String k){
+        return EncrypterDecrypter.decrypt(in, k);
+    }
     public static void specificrpca(DataInputStream in, String check, DataOutputStream out, String send) throws IOException {
         String temp = in.readUTF();
         while (!temp.equals(check)) {
@@ -161,17 +175,27 @@ public class SecureLineTP {
         return in.readUTF();
     }
 
-    public static void carpoutput(DataInputStream in, DataOutputStream out, String endoftransmition) throws IOException {
+    public static void rpcafinaloutputs(DataInputStream in, DataOutputStream out, String endoftransmition, @Nullable String k) throws IOException {
         //System.out.println(in.skipBytes(14));
         int readmode = Integer.parseInt(in.readUTF());
         System.out.println("Readmode" + readmode);
-        handleReadModes(in, out, readmode, endoftransmition);
+        handleReadModes(in, out, readmode, endoftransmition, 0, k);
         System.out.println("End of Transmition");
 
 
     }
 
-    public static void handleReadModes(DataInputStream in, DataOutputStream out, int rm, String eot) throws IOException {
+    public static String rpcafinaloutputs(DataInputStream in, DataOutputStream out, String endoftransmition, int obg, @Nullable String k) throws IOException {
+        int readmode = Integer.parseInt(in.readUTF());
+        System.out.println("Readmode" + readmode);
+        String huhu = handleReadModes(in, out, readmode, endoftransmition, obg, k);
+        System.out.println("End of Transmition");
+        return huhu;
+
+    }
+
+    public static String handleReadModes(DataInputStream in, DataOutputStream out, int rm, String eot, int obg, @Nullable String k) throws IOException {
+        String o0 = "";
         switch (rm) {
             case (1): {
                 String temp = "";
@@ -181,6 +205,9 @@ public class SecureLineTP {
                     if (outputinputrpcaverbose(in, out, ("Recived Package Number: " + pkgnum)).equals("Sending Package Number: " + pkgnum)) {
                         pkgnum++;
                         temp = in.readUTF();
+                        if(k != null){
+                            temp = keylayero(temp, k);
+                        }
                         collection += temp;
                     } else {
                         break;
@@ -222,12 +249,14 @@ public class SecureLineTP {
                         }
                         sum = ArrayUtils.addAll(sum, bytes);
                     }
-                    //sum = trimLeadingZeros(sum);
+                    if(k != null) {
+                        String hum = keylayero(Arrays.toString(sum), k);
+                        sum = keylayero(hum, k).getBytes();
+                    }
                     System.out.println("read it ALL");
-                    Files.write(of, sum);
+                    Path ppwpe = Files.write(of, sum);
                     temp = "";
                     while (temp.equals("")) {
-                        //System.out.println("Blanco");
                         temp = outputinputcarp(in, out, "Ticky");
                     }
                     System.out.println(temp);
@@ -248,12 +277,12 @@ public class SecureLineTP {
                 different = different % secondsInMilli;
                 long elapsedDeciSeconds = different / 10;
                 System.out.printf("Time elapsed: %d:%d:%d:%d%n", elapsedHours, elapsedMinutes, elapsedSeconds, elapsedDeciSeconds);
-
             }
             case (3): {
 
             }
         }
+        return "";
     }
 
     public static void rpcainputwhileverbose(DataInputStream in, DataOutputStream out, String endoftransmition) throws IOException {
@@ -277,6 +306,28 @@ public class SecureLineTP {
 
     }
 
+    public static void rpcainputsetwhileverbose(DataInputStream in, DataOutputStream out, String[] inputs, String endoftransmition) throws IOException {
+        String temp = in.readUTF();
+        boolean dumbfirstcheck = false;
+        if (temp.equals(endoftransmition)) {
+            System.out.println(temp);
+        }
+        int i = 0;
+        while (!temp.equals(endoftransmition)) {
+            if (dumbfirstcheck) {
+                temp = in.readUTF();
+            } else {
+                dumbfirstcheck = true;
+            }
+            System.out.println(temp);
+            if (!temp.equals(endoftransmition)) {
+                ca(out, inputs[i]);
+                i++;
+            }
+        }
+
+    }
+
     public static int specificrpcacontinuousinverboseexitcode(DataInputStream in, String check, DataOutputStream out) throws IOException {
         Scanner s = new Scanner(System.in);
         out.writeUTF(s.next());
@@ -292,22 +343,28 @@ public class SecureLineTP {
         return 0;
     }
 
-    public static void carpfile(DataInputStream in, DataOutputStream out, String endoftransmition, byte[] fin) throws IOException {
+    public static void carpfile(DataInputStream in, DataOutputStream out, String endoftransmition, byte[] fin, @Nullable String k) throws IOException {
         ca(out, "" + 2);
         specificrpcasendverboseverbose(in, "Ticky", out, "Begin Loop");
         specificrpcasendverboseverbose(in, "Ticky", out, "Continue");
-        handleWriteModes(in, out, 2, fin);
+        handleWriteModes(in, out, 2, fin, k);
         specificrpcasendverboseverbose(in, "Ticky", out, endoftransmition);
     }
 
-    public static boolean carprun(DataInputStream in, DataOutputStream out, String endoftransmition) throws IOException {
-        String temp = in.readUTF();
-        System.out.println(temp);
-        while (!Enforcry.SLcommands.containsKey(temp.toLowerCase())) {
-            caverbose(out, "Unknown Command");
+    public static boolean carprun(DataInputStream in, DataOutputStream out, String endoftransmition, @Nullable String commandIn) throws IOException {
+        String temp;
+        if(commandIn == null) {
             temp = in.readUTF();
+            System.out.println(temp);
+            while (!Enforcry.SLcommands.containsKey(temp.toLowerCase())) {
+                caverbose(out, "Unknown Command");
+                temp = in.readUTF();
+            }
+            caverbose(out, "Command Accepted");
         }
-        caverbose(out, "Command Accepted");
+        else {
+            temp =commandIn;
+        }
 
         AtomicInteger pkgnum = new AtomicInteger(1);
 
@@ -322,36 +379,51 @@ public class SecureLineTP {
         caverbose(out, "End of Inputs");
         command.run();
         ca(out, "" + command.outbytecode());
+        AtomicReference<String[]> ewe = new AtomicReference<>(new String[0]);
         specificrpcasendverboseverbose(in, "Ticky", out, "Begin Loop");
         command.getOutput().forEach((e) -> {
             try {
                 specificrpcasendverboseverbose(in, "Ticky", out, "Continue");
-                handleWriteModes(in, out, command.outbytecode(), e);
+                ewe.set(handleWriteModes(in, out, command.outbytecode(), e, null));
                 pkgnum.getAndIncrement();
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
         });
+        if(command.outbytecode() == 3) {
+            o3ut = ewe.get();
+        }
         specificrpcasendverboseverbose(in, "Ticky", out, endoftransmition);
         System.out.println("End of Transmition");
         System.out.println("Command " + command.getName() + " executed");
         return command.getCloseflag();
 
     }
-    public static void handleWriteModes(DataInputStream in, DataOutputStream out, int wm, Object e) throws IOException {
+    public static @NotNull String[] handleWriteModes(DataInputStream in, DataOutputStream out, int wm, Object e, String k) throws IOException {
         if (wm == 1) {
-            out.writeUTF((String) e);
+            String ee = "";
+            if(k != null){
+                ee = keylayeri(String.valueOf(e), k);
+            }
+            else{
+                ee = (String) e;
+            }
+            out.writeUTF(ee);
             out.flush();
         } else if (wm == 2) {
             int count;
-            InputStream is = new ByteArrayInputStream((byte[]) e);
+            byte[] ee;
+            if(k != null){
+                ee = keylayeri(String.valueOf(e), k).getBytes();
+            }
+            else{
+                ee = (byte[]) e;
+            }
+            InputStream is = new ByteArrayInputStream((byte[]) ee);
             DataInputStream inn = new DataInputStream(is);
             byte[] buffer = new byte[BTC];
             int lastcount  = 0;
             while ((count = inn.read(buffer)) > 0) {
-                //System.out.println("wreeted " + count);
-
-
                 out.write(buffer, 0, BTC);
                 out.flush();
             }
@@ -363,6 +435,9 @@ public class SecureLineTP {
 
             System.out.println("free bird");
 
+        } else if (wm == 3) {
+            return (String[]) e;
         }
+        return null;
     }
 }
