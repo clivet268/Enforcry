79f331e34082d6529580fe8efb85144fb13c408c
clivet268 <clivet268@gmail.com>
2022-11-30 17:54:57 -0500

    Cleanup

--- src/main/java/clivet268/SecureLine/Commands/ExacutableCommand.java
+++ src/main/java/clivet268/SecureLine/Commands/ExacutableCommand.java
@@ -42,10 +42,8 @@ public abstract class ExacutableCommand implements Runnable{
     /**
      * Runs the function stored inside and populates the output array if there are outputs
      */
-
     @Override
     public void run() {
-        this.output.add("default".getBytes());
     }
 
     /**
