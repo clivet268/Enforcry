7341e7c7ea48ab85adf359f12cc76c52dba39212
clivet268 <clivet268@gmail.com>
2022-12-02 15:05:38 -0500

    Switch changed to enhanced switch

--- src/main/java/clivet268/SecureLine/EFCTP.java
+++ src/main/java/clivet268/SecureLine/EFCTP.java
@@ -48,16 +48,16 @@ public class EFCTP {
     //TODO command # spoofing prevent with prompt number? must be 0
     //TODO Specifc error on code 13, use try catch and on recive get stacktrace string and print out (maybe)
     public boolean switcherServer(int ic) throws IOException, InterruptedException {
-        switch (ic){
+        switch (ic) {
             //TODO what is this?
-            case(2): {
+            case (2) -> {
                 o.writeInt(1);
                 o.flush();
-                break;
             }
+
             //TODO variable buffer size?
-            case(3): {
-                for(byte[] currentOut : es.getOutput()) {
+            case (3) -> {
+                for (byte[] currentOut : es.getOutput()) {
                     logger.log(Level.INFO, "193regrebrebebrerbabre2");
                     o.writeInt(18);
                     o.flush();
@@ -69,41 +69,39 @@ public class EFCTP {
                 }
                 o.writeInt(4);
                 o.flush();
-                break;
             }
-            case(7): {
+            case (7) -> {
                 logger.log(Level.INFO, "1");
                 String k = "";
                 //Checking validity, re-ask if not a valid command
-                while (!Enforcry.SLcommands.containsKey(k)){
+                while (!Enforcry.SLcommands.containsKey(k)) {
                     logger.log(Level.INFO, "102");
                     o.writeInt(6);
                     o.flush();
                     logger.log(Level.INFO, "1932");
                     k = i.readUTF().toLowerCase();
                 }
-                es =  Enforcry.SLcommands.get(k);
+                es = Enforcry.SLcommands.get(k);
                 logger.log(Level.INFO, "332");
-                if(es.commandPrompts().size() > 0) {
+                if (es.commandPrompts().size() > 0) {
                     logger.log(Level.INFO, "fewo");
                     o.writeInt(8);
                     o.flush();
-                }
-                else {
+                } else {
                     logger.log(Level.INFO, "121");
                     es.run();
                 }
-                if (es.getOutput().size() > 0){
+                if (es.getOutput().size() > 0) {
                     logger.log(Level.INFO, "2z2onihubhouou");
                     //Debug only
-                    System.out.println( new String(es.getOutput().get(0)));
+                    System.out.println(new String(es.getOutput().get(0)));
                     o.writeInt(2);
                     o.flush();
                     logger.log(Level.INFO, "uhhwfae9h298923bubyufg");
                 }
                 //TODO uhhh wont this break if there are actual files to send? i elseifed it but still maybe?
                 //TODO just add a flag for now
-                else if(es.getTnt() == 1){
+                else if (es.getTnt() == 1) {
                     logger.log(Level.INFO, "mai223douou");
                     //TODO More formal text mode? GUI? this whole thing needs a GUI thats going to suck :(
                     //TODO currently there is ping-pong-packet, maybe a ping-pong-packet-check is in need?
@@ -111,18 +109,16 @@ public class EFCTP {
                     // security involved such as a changing value used to encrypt again
                     o.writeInt(16);
                     o.flush();
-                }
-                else{
+                } else {
                     logger.log(Level.INFO, "w9fj9jwe");
                     //TODO else?
                 }
                 logger.log(Level.INFO, "maidouou");
-                break;
             }
-            case(12):{
+            case (12) -> {
                 int pnum = 0;
                 logger.log(Level.INFO, "qoiqw");
-                while(pnum < es.commandPrompts().size()){
+                while (pnum < es.commandPrompts().size()) {
                     logger.log(Level.INFO, "tj990");
                     o.writeInt(8);
                     o.flush();
@@ -133,30 +129,28 @@ public class EFCTP {
                 }
                 es.input = inputedStrings;
                 es.run();
-                if (es.getOutput().size() > 0){
+                if (es.getOutput().size() > 0) {
                     //Debug only
-                    System.out.println( new String(es.getOutput().get(0)));
+                    System.out.println(new String(es.getOutput().get(0)));
                     logger.log(Level.INFO, "uenique14918");
                     o.writeInt(2);
                     o.flush();
                 }
                 logger.log(Level.INFO, "232323");
-                break;
             }
-            case(16):{
+            case (16) -> {
                 System.out.println("Texting");
                 TextListener t = new TextListener(i, o, scanner);
-                Thread tthread = new Thread(t,"Server");
+                Thread tthread = new Thread(t, "Server");
                 tthread.start();
                 //TODO is this broken to need this?
                 //Kick the client into action
                 o.writeInt(14);
                 o.flush();
                 o.writeUTF("");
-                synchronized (lock){
+                synchronized (lock) {
                     lock.wait();
                 }
-                break;
             }
         }
         logger.log(Level.INFO, "ewnjowfuhofio");
@@ -170,60 +164,55 @@ public class EFCTP {
 
     //Client needs to flush!!!
     public boolean switcherClient(int ic) throws IOException, InterruptedException {
-        switch (ic){
-            case(2): {
+        switch (ic) {
+            case (2) -> {
                 o.writeInt(3);
                 o.flush();
-                break;
             }
-            case(4):{
+            case (4) -> {
                 //TODO uhhhh what to do here
                 logger.log(Level.INFO, "nio3ogg43iooi34oig34");
                 o.writeInt(7);
                 o.flush();
-                break;
             }
-            case(6): {
+            case (6) -> {
                 logger.log(Level.INFO, "2312");
                 System.out.println("Enter Command:");
                 String strin = scanner.nextLine();
                 o.writeUTF(strin);
                 logger.log(Level.INFO, "2443312");
-                break;
             }
-            case(8):{
+            case (8) -> {
                 logger.log(Level.INFO, "q23r223qw");
                 System.out.println(i.readUTF());
                 o.writeInt(12);
                 o.flush();
                 logger.log(Level.INFO, "q332fffqw");
                 o.writeUTF(scanner.nextLine());
-                break;
             }
-            case(16):{
+            case (16) -> {
                 o.writeInt(16);
                 //TODO brokey? why need to do this
                 //Clear utf buffer
                 i.readUTF();
                 System.out.println("Texting");
-                TextListener t =new TextListener(i, o, scanner);
+                TextListener t = new TextListener(i, o, scanner);
                 logger.log(Level.INFO, "q332fffqw");
                 Thread tthread = new Thread(t, "Client");
                 tthread.start();
                 logger.log(Level.INFO, "q332fffqw");
-                synchronized (lock){
+                synchronized (lock) {
                     lock.wait();
                 }
                 logger.log(Level.INFO, "ewfew76543212345678");
-                break;
             }
-            case(18):{
+            case (18) -> {
                 //TODO big integer
                 logger.log(Level.INFO, "viberuoahuigyutg87");
                 int packetLength = i.readInt();
                 byte[] packet = new byte[packetLength];
                 logger.log(Level.INFO, "korefibo9932993932");
-                i.readFully(packet, 0 ,packetLength);
+                i.readFully(packet, 0, packetLength);
                 logger.log(Level.INFO, "ji3459hu4wiuunoigwuinouibhubu");
                 String rn = getrandname();
                 String fpath = enforcrytestpath + rn;
@@ -231,14 +220,13 @@ public class EFCTP {
                 Files.createFile(of);
                 Files.write(of, packet);
                 System.out.println("File saved to " + rn);
-                break;
             }
+
             //TODO needed? better way? right now the out just sends code 20 from the client class
-            case (20):{
+            case (20) -> {
                 logger.log(Level.INFO, "0");
                 o.writeInt(7);
                 o.flush();
-                break;
             }
         }
 
