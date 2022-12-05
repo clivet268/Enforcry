ece2c4b4c75c2968cc5096f92fa2f9ae70de19f7
clivet268 <clivet268@gmail.com>
2022-12-05 11:53:03 -0500

    Text listener now uses an interruptable buffer for if the connection closes

--- src/main/java/clivet268/SecureLine/TextListener.java
+++ src/main/java/clivet268/SecureLine/TextListener.java
@@ -55,20 +55,60 @@ public class TextListener implements Runnable{
             });
             logger.log(Level.INFO, "4r4r24444");
             rn.start();
-            while (!kflag){
-                logger.log(Level.INFO, "u227828723873287g7372");
-                o.writeInt(14);
-                o.flush();
-                String ts = scanner.nextLine();
-                logger.log(Level.INFO, "u9843h43843th943th98");
-                o.writeUTF(ts);
+            System.out.print(username + ": ");
+            while (!kflag) {
+                String ts = call();
+                kflag = interpretOut(ts);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        logger.log(Level.INFO, "fef3jhkj232323232");
-        synchronized (EFCTP.lock){
-            EFCTP.lock.notify();
+        } catch (IOException ignored) {
         }
     }
+    //Escape character for commands such as "/exit"
+    public boolean interpretOut(String tso) throws IOException {
+        if(tso.length() > 0) {
+            if (!(tso.toCharArray()[0] == '/')) {
+                o.writeInt(14);
+                o.flush();
+                //TODO why does this string need to be flushed and not others?
+                o.writeUTF(tso);
+                o.flush();
+                System.out.print(username + ": ");
+            } else if (tso.equals("/exit")) {
+                o.writeInt(23);
+                o.flush();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //TODO possible without busy waiting?
+    /**
+     * credit https://www.javaspecialists.eu/archive/Issue153-Timeout-on-Console-Input.html and
+     * https://stackoverflow.com/questions/4983065/how-to-interrupt-java-util-scanner-nextline-call - author djna
+     * https://stackoverflow.com/users/82511/djna
+     * @return
+     * @throws IOException
+     */
+    public String call() throws IOException {
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+        String input;
+        do {
+            try {
+                // wait until we have data to complete a readLine()
+                while (!br.ready()) {
+                    if(kflag){
+                        br.close();
+                    }
+                    Thread.sleep(100);
+                }
+                input = br.readLine();
+            } catch (InterruptedException e) {
+                return null;
+            }
+        } while ("".equals(input));
+        return input;
+    }
+
 }
