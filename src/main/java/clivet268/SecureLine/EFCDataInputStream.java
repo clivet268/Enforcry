22e4dccb6c8f911f86ed8321da4a7fcc51e85499
clivet268 <clivet268@gmail.com>
2022-12-06 00:51:16 -0500

    EFC encryption layer on IO Added

---
+++ src/main/java/clivet268/SecureLine/EFCDataInputStream.java
@@ -0,0 +1,106 @@
+package clivet268.SecureLine;
+
+import clivet268.Encryption.Asymmetric;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Arrays;
+
+import static clivet268.Enforcry.logger;
+
+public class EFCDataInputStream {
+    private static DataInputStream din = null;
+    private static PrivateKey privateKey = null;
+    private static PublicKey publicKey = null;
+
+    /**
+     * @param in    passthrough for the socket's data input stream
+     * @param pkin  the private key should be the key of the current user (ex. on client should be client's private key)
+     * @param pukin the public key should be the key of the communicating user (ex. on client should be server's private key)
+     */
+    public EFCDataInputStream(DataInputStream in, PrivateKey pkin, PublicKey pukin) {
+        din = in;
+        privateKey = pkin;
+        publicKey = pukin;
+    }
+/*
+    public final void readFullyE(byte b[]) throws IOException {
+        readFullyE(b, 0, b.length);
+    }
+
+    public final void readFullyE(byte b[], int off, int len) throws IOException {
+        Objects.checkFromIndexSize(off, len, b.length);
+        int n = 0;
+        while (n < len) {
+            int count = din.read(b, off + n, len - n);
+            if (count < 0)
+                throw new EOFException();
+            n += count;
+        }
+    }
+
+ */
+
+    public byte[] read(byte b[], int off, int len) throws Exception {
+        int codelen = din.readInt();
+        byte[] codine = new byte[codelen];
+        din.readFully(codine, 0, codelen);
+        logger.log(Arrays.toString(codine));
+        String ininfo = new String(Asymmetric.do_RSADecryption(codine, privateKey));
+        int type = Integer.parseInt(ininfo.substring(0, ininfo.indexOf(":")));
+        int msgleng = Integer.parseInt(ininfo.substring(ininfo.indexOf(":") + 1));
+        byte[] bytesin = new byte[msgleng];
+        System.out.println(type);
+        logger.log(type + ":" + msgleng);
+        logger.log(Arrays.toString((type + ":" + msgleng).getBytes()));
+        din.readFully(bytesin);
+        logger.log(Arrays.toString(bytesin));
+        return Asymmetric.do_RSADecryption(bytesin, privateKey);
+    }
+
+
+    public String readUTFE() throws Exception {
+        int codelen = din.readInt();
+        byte[] codine = new byte[codelen];
+        din.readFully(codine, 0, codelen);
+        logger.log(Arrays.toString(codine));
+        String ininfo = new String(Asymmetric.do_RSADecryption(codine, privateKey));
+        int type = Integer.parseInt(ininfo.substring(0, ininfo.indexOf(":")));
+        int msgleng = Integer.parseInt(ininfo.substring(ininfo.indexOf(":") + 1));
+        byte[] stringn = new byte[msgleng];
+        System.out.println(type);
+        logger.log(type + ":" + msgleng);
+        logger.log(Arrays.toString((type + ":" + msgleng).getBytes()));
+        din.readFully(stringn);
+        String strout = new String(Asymmetric.do_RSADecryption(stringn, privateKey));
+        logger.log(Arrays.toString(stringn));
+        return strout;
+        //TODO hash confirm?
+    }
+
+    public int readIntE() throws Exception {
+        int codelen = din.readInt();
+        byte[] codine = new byte[codelen];
+        din.readFully(codine, 0, codelen);
+        logger.log(Arrays.toString(codine));
+        String ininfo = new String(Asymmetric.do_RSADecryption(codine, privateKey));
+        int type = Integer.parseInt(ininfo.substring(0, ininfo.indexOf(":")));
+        int msgleng = Integer.parseInt(ininfo.substring(ininfo.indexOf(":") + 1));
+        byte[] stringn = new byte[msgleng];
+        System.out.println(type);
+        logger.log(type + ":" + msgleng);
+        logger.log(Arrays.toString((type + ":" + msgleng).getBytes()));
+        din.readFully(stringn);
+        logger.log(Arrays.toString(stringn));
+        String strout = new String(Asymmetric.do_RSADecryption(stringn, privateKey));
+        return Integer.parseInt(strout);
+
+        //TODO hash confirm?
+    }
+
+    public void close() throws IOException {
+        din.close();
+    }
+}
---
+++ src/main/java/clivet268/SecureLine/EFCDataOutputStream.java
@@ -0,0 +1,85 @@
+package clivet268.SecureLine;
+
+import clivet268.Encryption.Asymmetric;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Arrays;
+
+import static clivet268.Enforcry.logger;
+
+public class EFCDataOutputStream {
+    private static DataOutputStream din = null;
+    private static PrivateKey privateKey = null;
+    private static PublicKey publicKey = null;
+
+    public EFCDataOutputStream(DataOutputStream out, PrivateKey pkin, PublicKey pukin) {
+        din = out;
+        privateKey = pkin;
+        publicKey = pukin;
+    }
+
+    public void writeE(byte b[]) throws Exception {
+        byte[] bytesout = Asymmetric.do_RSAEncryption(b, publicKey);
+        byte[] codeOut = Asymmetric.do_RSAEncryption((103 + ":" + bytesout.length).getBytes(), publicKey);
+        din.writeInt(codeOut.length);
+        logger.log(103 + ":" + bytesout.length);
+        logger.log(Arrays.toString((103 + ":" + bytesout.length).getBytes()));
+        logger.log(Arrays.toString(codeOut));
+        din.write(codeOut);
+        logger.log(Arrays.toString(bytesout));
+        din.write(bytesout, 0, bytesout.length);
+    }
+
+
+    public final void writeUTFE(String str) throws Exception {
+        byte[] stringOut = Asymmetric.do_RSAEncryption(str.getBytes(str), publicKey);
+        /*
+        //TODO needed? already encrypted but the idea of this is small numbers in the hundreds could clash since are so small
+        // investigate this, maybe not needed
+        int verypseudorandompos = publicKey.getEncoded()[Enforcry.username.hashCode() % 127];
+        byte[] randmess = new byte[verypseudorandompos * (Enforcry.username.hashCode() % 10 + 1)];
+        new Random().nextBytes(randmess);
+        int postoplace = (int) (((double)Enforcry.username.hashCode() % 10) * 10.791 + (randmess.length/2)) % randmess.length;
+
+        List tot = new ArrayList(List.of(Arrays.copyOfRange(randmess, 0, postoplace)));
+         */
+
+        byte[] codeOut = Asymmetric.do_RSAEncryption((102 + ":" + stringOut.length).getBytes(), publicKey);
+        din.writeInt(codeOut.length);
+        logger.log(102 + ":" + stringOut.length);
+        logger.log(Arrays.toString((102 + ":" + stringOut.length).getBytes()));
+        logger.log(Arrays.toString(codeOut));
+        din.write(codeOut);
+        logger.log(Arrays.toString(stringOut));
+        din.write(stringOut);
+    }
+
+    public final void writeIntE(int intin) throws Exception {
+        byte[] stringOut = Asymmetric.do_RSAEncryption(BigInteger.valueOf(intin).toByteArray(), publicKey);
+        byte[] codeOut = Asymmetric.do_RSAEncryption((101 + ":" + stringOut.length).getBytes(), publicKey);
+        din.writeInt(codeOut.length);
+        logger.log(102 + ":" + stringOut.length);
+        logger.log(Arrays.toString((101 + ":" + stringOut.length).getBytes()));
+        logger.log(Arrays.toString(codeOut));
+        din.write(codeOut);
+        logger.log(Arrays.toString(stringOut));
+        din.write(stringOut);
+        flush();
+    }
+
+    //TODO build into write int and normal writes (not UTF), neede
+    public void flush() throws IOException {
+        din.flush();
+    }
+
+
+    public void close() throws IOException {
+        din.close();
+    }
+
+
+}
